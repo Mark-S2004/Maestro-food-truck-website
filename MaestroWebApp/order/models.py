6938c527741ea30b6c9f2528bd4d0db1bb4bd85c
Mark Saleh <mark-saleh@outlook.com>
2023-05-20 01:25:42 +0300

    Add MenuItems model

--- MaestroWebApp/order/models.py
+++ MaestroWebApp/order/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class MenuItems(models.Model):
+    name = models.CharField(max_length=128)
+    size = models.CharField(max_length=5, choices=[
+        ('150', '150G'),
+        ('200', '200G')
+    ], default='150G')
+    price = models.IntegerField()
