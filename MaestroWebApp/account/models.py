11df5317a58a7302c606fb7cee6af591e58d12d2
Mark Saleh <mark-saleh@outlook.com>
2023-05-19 20:31:41 +0300

    Create Users model

--- MaestroWebApp/account/models.py
+++ MaestroWebApp/account/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+class Users(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.EmailField()
+    password = models.CharField(max_length=128, validators=[MinLengthValidator(8)])
+    phone = PhoneNumberField(null=True, blank=True, unique=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
